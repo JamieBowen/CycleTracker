7fe1db3ff7ac4fd41d3a2aafd25ec7dd4647487a
jbeck <jbeck8176@gmail.com>
2016-12-21 11:28:08 -0500

    added data project

---
+++ src/CycleTracker.Data/Models/Part.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CycleTracker.Data.Models
+{
+    public class Part
+    {
+	    public Guid Id { get; set; }
+	    public string Name { get; set; }
+	    public string Description { get; set; }
+	    public DateTimeOffset InstallationDate { get; set; }
+	    public int InstallationBikeMilage { get; set; }
+	    public DateTimeOffset ReplacedDate { get; set; }
+	    public int ReplacedBikeMilage { get; set; }
+	    public int AccurredMilage { get; set; }
+	    public string Retailer { get; set; }
+    }
+}
