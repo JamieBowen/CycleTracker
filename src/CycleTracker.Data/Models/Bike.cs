690824088326e59925b9be4bc87c554e31d3558b
jbowen015 <jbowen@renovo1.com>
2016-12-21 13:16:06 -0500

    Add bike model properties

--- src/CycleTracker.Data/Models/Bike.cs
+++ src/CycleTracker.Data/Models/Bike.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CycleTracker.Data.Models
 {
-    public class Bike
-    {
-    }
+	public class Bike
+	{
+		public Guid Id { get; set; }
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+		public string Trim { get; set; }
+		public string Colors { get; set; }
+	}
 }
