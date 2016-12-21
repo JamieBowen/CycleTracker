a33dde687c2ee195ccb26834c34592488158aa30
jbeck <jbeck8176@gmail.com>
2016-12-21 13:18:11 -0500

    maybe?

---
+++ src/CycleTracker.Data/Repositories/BikeRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CycleTracker.Data.Repositories
+{
+    public class BikeRepository : SqLiteBaseRepository
+    {
+	    public void getAll()
+	    {
+		    if (!File.Exists(DbFileLocation))
+		    {
+			    CreateDatabase();
+		    }
+	    }
+
+	    private static void CreateDatabase()
+	    {
+		    using (var cnn = CycleTrackerDbConnection())
+		    {
+			    cnn.Open();
+			    cnn.Execute(
+					@"create table Bikes
+                    (
+						Id                  UNIQUEIDENTIFIER primary key,
+                        Make                varchar(100) not null,
+                        Model               varchar(100) not null,
+                        Year                int not null,
+						Trim	            varchar(100) null,
+						ColorDescription	varchar(200) null
+                      )");
+		    }
+	    }
+    }
+}
---
+++ src/CycleTracker.Data/Repositories/SqlLiteBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace CycleTracker.Data.Repositories
+{
+    public abstract class SqLiteBaseRepository
+    {
+	    public static string DbFileLocation => Directory.GetCurrentDirectory() + "CycleTrackerData.sqlite";
+
+	    public static SqliteConnection CycleTrackerDbConnection()
+	    {
+			return new SqliteConnection("Data Source=" + DbFileLocation);
+	    }
+    }
+}
